32e8d9eaba3638c712c3831febb535bde3a6da2f
rwedzony <rwedzony@gmail.com>
2021-02-17 22:40:02 +0100

    added validations to attributes and model test cases

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,2 +1,4 @@
 class Form < ApplicationRecord
+validates :first_name, :birth_date,  presence: true
+validates :something_about, length: {maximum: 140}
 end
--- test/models/form_test.rb
+++ test/models/form_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class FormTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup 
+    @form = Form.new()
+  end
+
+   test "form should not be valid without given first name" do
+     assert_not @form.valid?
+   end
+
+   test "form should not be valid without given birth date" do
+     @form.first_name="Sampledata"
+     assert_not @form.valid?
+   end
+
+   test "Something_about attribute can be blank" do
+     @form.first_name="Sampledata"
+     @form.birth_date="18.01.1991"
+     assert @form.valid?
+   end
+
+   test "Something_about attribute should be top 140 characters long" do
+     @form.first_name="Sampledata"
+     @form.birth_date="18.01.1991"
+     @form.something_about="a"*141
+     assert_not @form.valid?
+   end
+
 end
